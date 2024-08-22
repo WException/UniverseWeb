316c92c7d1682af8db5fba64d073cb3662dd104d
Zhaoyu <1983909571@qq.com>
2024-08-22 16:24:41 +0800

    init

---
+++ nicey-admin/src/main/java/work/nicey/RuoYiApplication.java
@@ -0,0 +1,23 @@
+package work.nicey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+/**
+ * 启动程序
+ * 
+ * @author ruoyi
+ */
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class RuoYiApplication
+{
+    public static void main(String[] args)
+    {
+        // System.setProperty("spring.devtools.restart.enabled", "false");
+        SpringApplication.run(RuoYiApplication.class, args);
+        System.out.println("后端服务启动成功");
+    }
+
+}
---
+++ nicey-common/src/main/java/work/nicey/common/core/domain/model/RegisterBody.java
@@ -0,0 +1,11 @@
+package work.nicey.common.core.domain.model;
+
+/**
+ * 用户注册对象
+ * 
+ * @author ruoyi
+ */
+public class RegisterBody extends LoginBody
+{
+
+}
