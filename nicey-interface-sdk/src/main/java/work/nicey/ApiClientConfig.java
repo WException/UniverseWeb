a62f29e4cf22aac5b11321c9db3bde3ea779f542
Zhaoyu <1983909571@qq.com>
2024-10-14 17:13:25 +0800

    feat(nicey-interface-sdk): sdk模块基础框架搭建，简单的打包调用测试通过

---
+++ nicey-interface-sdk/src/main/java/work/nicey/ApiClientConfig.java
@@ -0,0 +1,44 @@
+package work.nicey;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import work.nicey.client.ApiClient;
+
+/**
+ * Api 客户端配置
+ *
+ * @author <a href="https://nicey.work/">尼丝</a>
+ */
+@Configuration
+@ConfigurationProperties("nicey.api.client")
+@ComponentScan
+public class ApiClientConfig {
+
+    private String accessKey;
+
+    private String secretKey;
+
+    @Bean
+    public ApiClient ApiClient() {
+        return new ApiClient(accessKey, secretKey);
+    }
+
+    public String getAccessKey() {
+        return accessKey;
+    }
+
+    public void setAccessKey(String accessKey) {
+        this.accessKey = accessKey;
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+}
+
---
+++ nicey-interface-sdk/src/main/java/work/nicey/utils/SignUtils.java
@@ -0,0 +1,23 @@
+package work.nicey.utils;
+
+import cn.hutool.crypto.digest.DigestAlgorithm;
+import cn.hutool.crypto.digest.Digester;
+
+/**
+ * 签名工具
+ *
+ * @author <a href="https://nicey.work/">尼丝</a>
+ */
+public class SignUtils {
+    /**
+     * 生成签名
+     * @param body
+     * @param secretKey
+     * @return
+     */
+    public static String genSign(String body, String secretKey) {
+        Digester md5 = new Digester(DigestAlgorithm.SHA256);
+        String content = body + "." + secretKey;
+        return md5.digestHex(content);
+    }
+}
